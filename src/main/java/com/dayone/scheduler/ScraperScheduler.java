5aa0d79f9daa628c90f7173834ffb539ac7433fd
nayoon <nayoonk928@gmail.com>
2023-05-30 23:37:00 +0900

    스케줄러

--- src/main/java/com/dayone/scheduler/ScraperScheduler.java
+++ src/main/java/com/dayone/scheduler/ScraperScheduler.java
@@ -33,15 +33,19 @@ public class ScraperScheduler {
     @CacheEvict(value = CacheKey.KEY_FINANCE, allEntries = true)
     @Scheduled(cron = "${scheduler.scrap.yahoo}")
     public void yahooFinanceScheduling() {
-        log.info("scraping scheduler is started");
+        //log.info("scraping scheduler is started");
         // 저장된 회사 목록을 조회
         List<CompanyEntity> companies = this.companyRepository.findAll();
 
         // 회사마다 배당금 정보를 새로 스크래핑
         for (var company : companies) {
             log.info("scraping scheduler is started -> " + company.getName());
-            ScrapedResult scrapedResult = this.yahooFinanceScraper.scrap(
-                                                        new Company(company.getTicker(), company.getName()));
+            ScrapedResult scrapedResult =
+                    this.yahooFinanceScraper.scrap (
+                            Company.builder()
+                                    .name(company.getName())
+                                    .ticker(company.getTicker())
+                                    .build());
 
             // 스크래핑한 배당금 정보 중 데이터베이스에 없는 값은 저장
             scrapedResult.getDividends().stream()
@@ -49,8 +53,12 @@ public class ScraperScheduler {
                     .map(e -> new DividendEntity(company.getId(), e))
                     // 엘리먼트를 하나씩 디비든 레파지토리에 삽입
                     .forEach(e -> {
-                        // do something
-                        throw new NotYetImplementedException();
+                        boolean exists = this.dividendRepository
+                                .existsByCompanyIdAndDate(e.getCompanyId(), e.getDate());
+                        if (!exists) {
+                            this.dividendRepository.save(e);
+                        }
+                        //throw new NotYetImplementedException();
                     });
 
             // 연속적으로 스크래핑 대상 사이트 서버에 요청을 날리지 않도록 일시정지
